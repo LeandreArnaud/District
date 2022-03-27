e3b3c3a7b75730404a3143b4d53b282cac94422e
LeandreArnaud <pro.leandre.arnaud@gmail.com>
2022-03-27 19:36:18 +0200

    creates homepage layout

--- screens/Homepage.tsx
+++ screens/Homepage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native'
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native'
 import text from '../assets/text/text-fr.json'
 
 
@@ -10,7 +10,10 @@ export const Homepage: React.FC<HomePageProps> = ({ navigation }) => {
     return(
         <View style={styles.mainContainer}>
             <View style={styles.header}>
-                <Text>icon</Text>
+                <Image
+                    style={styles.headerIcon}
+                    source={require('../assets/icons/hydrant.png')}
+                />
                 <View style={styles.headerTextContainer}>
                     <Text style={styles.h1}>{text.homepage.title}</Text>
                     <Text style={styles.h2}>{text.homepage.subtile}</Text>
@@ -19,8 +22,12 @@ export const Homepage: React.FC<HomePageProps> = ({ navigation }) => {
 
             <View style={styles.toolsContainer}>
             {text.homepage.tools.map(tool => 
-                <TouchableOpacity style={styles.toolCardBody} onPress={()=>navigation.navigate('DistrictCreator')}>
-                    <Text>{tool.icon}</Text>
+                <TouchableOpacity style={styles.toolCardBody} onPress={()=>navigation.navigate('DistrictCreator')} key={tool.title}>
+                    {/* <Text style={styles.toolIcon}>{tool.icon}</Text> */}
+                    <Image
+                        style={styles.toolIcon}
+                        source={require('../assets/icons/earth.png')}
+                    />
                     <View style={styles.toolCardBodyRight}>
                         <Text style={styles.toolTitle}>{tool.title}</Text>
                         <Text style={styles.toolSubtitle}>{tool.description}</Text>
@@ -41,20 +48,33 @@ const styles = StyleSheet.create({
         paddingTop: 40,
     },
     header: {
-        backgroundColor: "green",
         flex: 0,
         flexDirection: "row",
         alignItems: "center",
         height: 80,
     },
+    headerIcon: {
+        height: "70%",
+        width: 50,
+        resizeMode: "center",
+    },
     headerTextContainer: {
-        backgroundColor: "red",
+        height: "100%",
+        flex: 0,
+        alignItems: "center",
     },
     h1: {
+        color: "#DAAC08",
         fontSize: 40,
+        fontWeight: "bold",
+        letterSpacing: 2,
     },
     h2: {
-        fontSize: 20,
+        color: "#DAAC08",
+        fontSize: 25,
+        fontWeight: "bold",
+        letterSpacing: 2,
+        lineHeight: 25,
     },
     toolsContainer: {
         alignItems: 'center',
@@ -62,24 +82,34 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     toolCardBody: {
-        backgroundColor: "yellow",
+        backgroundColor: "#FFFFFF",
         flex: 0,
         flexDirection: "row",
         alignItems: "center",
         justifyContent: 'center',
-        height: 80,
-        width: '90%',
-        borderRadius: 10,
+        paddingVertical: 15,
+        width: '95%',
+        borderRadius: 6,
         overflow: "hidden",
+        elevation: 4,
+        shadowColor: "#000000",
+    },
+    toolIcon: {
+        width: '20%',
+        height: 40,
+        resizeMode: 'center',
     },
     toolCardBodyRight: {
-        backgroundColor: "pink",
+        width: '80%',
+        paddingRight: 10,
     },
     toolTitle: {
         fontSize: 20,
+        fontWeight: "700",
+        paddingBottom: 3,
     },
     toolSubtitle: {
-        fontSize: 15,
+        fontSize: 13,
     },
 });
 
