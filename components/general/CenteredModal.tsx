ded6e2c36208a421e4a3b368c5dad070d138a466
LeandreArnaud <pro.leandre.arnaud@gmail.com>
2022-05-08 20:49:46 +0200

    creates generic centered modal

---
+++ components/general/CenteredModal.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {StyleSheet, View, TouchableWithoutFeedback, Text} from 'react-native'
+
+type CenteredModalProps = { hideModal: () => void, children: React.ReactNode};
+
+export const CenteredModal: React.FC<CenteredModalProps> = ({children, hideModal}) => {
+
+    return(
+        <View style={styles.container}>
+            <TouchableWithoutFeedback onPress={hideModal}>
+                <View style={styles.backgroundBlur} />
+            </TouchableWithoutFeedback>
+            <View style={styles.modalContentContainer}>
+                {children}
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        position: "absolute",
+        zIndex: 6,
+        width: "100%",
+        height: "110%",
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    backgroundBlur: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#000000",
+        opacity: 0.4,
+        position: "absolute",
+        top: 0,
+    },
+    modalContentContainer: {
+        backgroundColor: "#FFFFFF"
+    }
+});
+
+
+export default CenteredModal
