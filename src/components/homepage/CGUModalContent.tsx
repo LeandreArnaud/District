ffafdace149d433f3e59eb2abebc35ad7209a084
LeandreArnaud <pro.leandre.arnaud@gmail.com>
2022-05-20 00:06:50 +0200

    feat: uses a CGU json

--- src/components/homepage/CGUModalContent.tsx
+++ src/components/homepage/CGUModalContent.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {StyleSheet, View, Text} from 'react-native'
 import text from '../../../assets/text/text-fr.json'
 import { saveAcceptedCGU } from '../../services/LocalStorage';
+import cguText from '../../../assets/text/cgu.json'
+import { ScrollView } from 'react-native-gesture-handler';
 
 type CGUModalContentProps = {
     onCloseModal: () => void
@@ -9,23 +11,46 @@ type CGUModalContentProps = {
 
 export const CGUModalContent: React.FC<CGUModalContentProps> = ({onCloseModal}) => {
 
+    // disable for now to avoid user blocked on modal
+    const [hasScrollToBottom, setHasScrollToBottom] = useState<boolean>(true);
+
     const handleAcceptedButton = async () => {
-        await saveAcceptedCGU();
-        onCloseModal();
+        if (hasScrollToBottom) {
+            await saveAcceptedCGU();
+            onCloseModal();
+        }
     }
 
     return(
         <View style={styles.container}>
             <View style={styles.body}>
-                <Text style={styles.h1}>CGU</Text>
-                <Text style={styles.p}>content</Text>
+                <ScrollView onScrollEndDrag={() => setHasScrollToBottom(true)}>
+                    <View>
+                        <Text style={styles.h1}>{text.CGU.cgu.title}</Text>
+                        {Object.keys(cguText).map((key, index) => 
+                            <View key={`view-${index}`}>
+                                <Text style={styles.h2} key={`${index}`}>{key}</Text>
+                                {Object.values(cguText)[index].map((para, subindex) => 
+                                    <Text style={styles.p} key={`${subindex}-${index}`}>{para}</Text>
+                                )}
+                            </View>
+                        )}
+                        <View style={styles.bottomMargin}/>
+                    </View>
+                </ScrollView>
             </View>
             <View style={styles.buttonsContainer}>
-                <View style={styles.button} onTouchEnd={handleAcceptedButton}>
-                    <Text style={styles.buttonText}>Accepter</Text>
+                <View style={hasScrollToBottom 
+                    ? styles.button 
+                    : {...styles.button, backgroundColor:'#CCCCCC'}} 
+                onTouchEnd={handleAcceptedButton}>
+                    <Text style={ hasScrollToBottom 
+                        ? styles.buttonText
+                        : {...styles.buttonText, color:'gray'}}>
+                            {text.CGU.cgu.acceptedButton}
+                    </Text>
                 </View>
             </View>
-            
         </View>
     );
 };
@@ -43,6 +68,11 @@ const styles = StyleSheet.create({
     },
     body: {
         width: '90%',
+        height: '100%',
+        overflow: 'hidden',
+    },
+    bottomMargin: {
+        marginBottom: 60,
     },
     h1: {
         fontSize: 40,
@@ -51,17 +81,25 @@ const styles = StyleSheet.create({
         marginBottom: 40,
         marginTop: 20,
     },
+    h2: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 10,
+    },
     p: {
         fontSize: 15,
         lineHeight: 22,
         textAlign: 'justify',
     },
     buttonsContainer: {
+        position: 'absolute',
+        bottom: 0,
         height: 50,
         width: '100%',
         flex: 0,
         flexDirection: 'row',
-        justifyContent: 'space-evenly'
+        justifyContent: 'space-evenly',
     },
     button: {
         backgroundColor: '#DAAC08',
@@ -75,6 +113,7 @@ const styles = StyleSheet.create({
     },
     buttonText: {
         fontWeight: 'bold',
+        color: 'black'
     }
 });
 
